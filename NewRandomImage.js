7f2bf7ab07d2022c5e811d4b381754123b01c025
RamonaPrower <garfieldfan3993@gmail.com>
2014-10-30 18:08:50 +0000

    Youtube API Integration, Fully Filled out Arrays
    also added soundcloud support and SongName()

--- NewRandomImage.js
+++ NewRandomImage.js
@@ -17,37 +17,37 @@ theImages[13] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/14.png'
 theImages[14] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/15.png'
 theImages[15] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/16.png'
 theImages[16] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/17.png'
-//theImages[17] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/18.png'
-//theImages[18] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/19.png'
-//theImages[19] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/20.png'
-//theImages[20] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/21.png'
-//theImages[21] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/22.png'
-//theImages[22] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/23.png'
-//theImages[23] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/24.png'
-//theImages[24] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/25.png'
-//theImages[25] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/26.png'
-//theImages[26] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/27.png'
-//theImages[27] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/28.png'
-//theImages[28] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/29.png'
-//theImages[29] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/30.png'
-//theImages[30] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/31.png'
-//theImages[31] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/32.png'
-//theImages[32] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/33.png'
-//theImages[33] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/34.png'
-//theImages[34] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/35.png'
-//theImages[35] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/36.png'
-//theImages[36] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/37.png'
-//theImages[37] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/38.png'
-//theImages[38] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/39.png'
-//theImages[39] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/40.png'
-//theImages[40] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/41.png'
-//theImages[41] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/42.png'
-//theImages[42] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/43.png'
-//theImages[43] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/44.png'
-//theImages[44] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/45.png'
-//theImages[45] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/46.png'
-//theImages[46] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/47.png'
-//theImages[47] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/48.png'
+theImages[17] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/18.png'
+theImages[18] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/19.png'
+theImages[19] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/20.png'
+theImages[20] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/21.png'
+theImages[21] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/22.png'
+theImages[22] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/23.png'
+theImages[23] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/24.png'
+theImages[24] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/25.png'
+theImages[25] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/26.png'
+theImages[26] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/27.png'
+theImages[27] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/28.png'
+theImages[28] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/29.png'
+theImages[29] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/30.png'
+theImages[30] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/31.png'
+theImages[31] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/32.png'
+theImages[32] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/33.png'
+theImages[33] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/34.png'
+theImages[34] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/35.png'
+theImages[35] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/36.png'
+theImages[36] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/37.png'
+theImages[37] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/38.png'
+theImages[38] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/39.png'
+theImages[39] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/40.png'
+theImages[40] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/41.png'
+theImages[41] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/42.png'
+theImages[42] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/43.png'
+theImages[43] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/44.png'
+theImages[44] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/45.png'
+theImages[45] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/46.png'
+theImages[46] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/47.png'
+theImages[47] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/Avvys/48.png'
 
 var theEmbedCode = new Array()
 
@@ -68,37 +68,37 @@ theEmbedCode[13] = 'jk2cGZPVW-g'
 theEmbedCode[14] = 'QL4O8adW9_w'
 theEmbedCode[15] = 'https://www.youtube.com/results?search_query=doge+meme'
 theEmbedCode[16] = 'cqPD4A-bKNA'
-theEmbedCode[17] = ''
-theEmbedCode[18] = ''
-theEmbedCode[19] = ''
-theEmbedCode[20] = ''
-theEmbedCode[21] = ''
-theEmbedCode[22] = ''
-theEmbedCode[23] = ''
-theEmbedCode[24] = ''
-theEmbedCode[25] = ''
-theEmbedCode[26] = ''
-theEmbedCode[27] = ''
-theEmbedCode[28] = ''
-theEmbedCode[29] = ''
-theEmbedCode[30] = ''
-theEmbedCode[31] = ''
-theEmbedCode[32] = ''
-theEmbedCode[33] = ''
-theEmbedCode[34] = ''
-theEmbedCode[35] = ''
-theEmbedCode[36] = ''
-theEmbedCode[37] = ''
-theEmbedCode[38] = ''
-theEmbedCode[39] = ''
-theEmbedCode[40] = ''
-theEmbedCode[41] = ''
-theEmbedCode[42] = ''
-theEmbedCode[43] = ''
-theEmbedCode[44] = ''
-theEmbedCode[45] = ''
-theEmbedCode[46] = ''
-theEmbedCode[47] = ''
+theEmbedCode[17] = 'feCXr86HzMk'
+theEmbedCode[18] = 'PS3jlUPPf5U'
+theEmbedCode[19] = 'PYtXuBN1Hvc'
+theEmbedCode[20] = '7pKXIBYmI0w'
+theEmbedCode[21] = 'vZezfqlV1r4'
+theEmbedCode[22] = 'TSTKLEKPu44'
+theEmbedCode[23] = 'Djr4jfHcslA'
+theEmbedCode[24] = 'EjH9cEoEup8'
+theEmbedCode[25] = 'dE-IoRk1qgk'
+theEmbedCode[26] = 'http://i.imgur.com/C4idW6A.jpg'
+theEmbedCode[27] = 'iDbyYGrswtg'
+theEmbedCode[28] = 'POZ4CcJILWw'
+theEmbedCode[29] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/CodeMonkey.swf'  //i will make a RenderFlash at some point but i'm too lazy today
+theEmbedCode[30] = 'tXjtATYG72w'
+theEmbedCode[31] = 'OlRgazC3UAE'
+theEmbedCode[32] = '8GFnOGPGoOA'
+theEmbedCode[33] = 'PSYxT9GM0fQ' //song name?
+theEmbedCode[34] = 'VX6KjdeI6KU'
+theEmbedCode[35] = 'd7r9RqWBdl8'
+theEmbedCode[36] = '157207895'   //Soundcloud URL from API
+theEmbedCode[37] = 'yzbQ_h_uuVc'
+theEmbedCode[38] = 'QMMceB61JH0'
+theEmbedCode[39] = 'utaWZT5N-tQ'
+theEmbedCode[40] = 'sJeBTCzILzw'
+theEmbedCode[41] = 'aboZctrHfK8'
+theEmbedCode[42] = 'http://rainbowdashalwaysdressesinstyle.com/Tumblr/8993.gif'
+theEmbedCode[43] = 'QwIDiJ_zd_k'
+theEmbedCode[44] = 'b1smWr5xrS8'
+theEmbedCode[45] = 'TKJLd_cQ068'
+theEmbedCode[46] = 'W9DST-6jIBU'
+theEmbedCode[47] = 'iEIlxdPXJUY'
 
 var theExtras = new Array()
 
@@ -126,9 +126,9 @@ theExtras[20] = ''
 theExtras[21] = ''
 theExtras[22] = ''
 theExtras[23] = ''
-theExtras[24] = ''
+theExtras[24] = '&start=75'
 theExtras[25] = ''
-theExtras[26] = ''
+theExtras[26] = 'style="width: 100%;"'
 theExtras[27] = ''
 theExtras[28] = ''
 theExtras[29] = ''
@@ -144,12 +144,12 @@ theExtras[38] = ''
 theExtras[39] = ''
 theExtras[40] = ''
 theExtras[41] = ''
-theExtras[42] = ''
+theExtras[42] = 'style="width: 350px;"'
 theExtras[43] = ''
-theExtras[44] = ''
-theExtras[45] = ''
-theExtras[46] = ''
-theExtras[47] = ''
+theExtras[44] = '&start=5&end=13'
+theExtras[45] = '&start=5&end=38'
+theExtras[46] = '&start=183&end=189'
+theExtras[47] = '&start=11&end=34'
 
 
 var theType = new Array()
@@ -171,37 +171,37 @@ theType[13] = 'RenderVideo()'
 theType[14] = 'RenderVideo()'
 theType[15] = 'RenderLink()'
 theType[16] = 'RenderVideo()'
-theType[17] = ''
-theType[18] = ''
-theType[19] = ''
-theType[20] = ''
-theType[21] = ''
-theType[22] = ''
-theType[23] = ''
-theType[24] = ''
-theType[25] = ''
-theType[26] = ''
-theType[27] = ''
-theType[28] = ''
-theType[29] = ''
-theType[30] = ''
-theType[31] = ''
-theType[32] = ''
-theType[33] = ''
-theType[34] = ''
-theType[35] = ''
-theType[36] = ''
-theType[37] = ''
-theType[38] = ''
-theType[39] = ''
-theType[40] = ''
-theType[41] = ''
-theType[42] = ''
-theType[43] = ''
-theType[44] = ''
-theType[45] = ''
-theType[46] = ''
-theType[47] = ''
+theType[17] = 'RenderVideo()'
+theType[18] = 'RenderVideo()'
+theType[19] = 'RenderVideo()'
+theType[20] = 'RenderVideo()'
+theType[21] = 'RenderVideo()'
+theType[22] = 'RenderVideo()'
+theType[23] = 'RenderVideo()'
+theType[24] = 'RenderVideo()'
+theType[25] = 'RenderVideo()'
+theType[26] = 'RenderImage()'
+theType[27] = 'RenderVideo()'
+theType[28] = 'RenderVideo()'
+theType[29] = 'RenderLink()' //consider making a RenderFlash for this
+theType[30] = 'RenderVideo()'
+theType[31] = 'RenderVideo()'
+theType[32] = 'RenderVideo()'
+theType[33] = 'RenderVideo();DelaySongName()'
+theType[34] = 'RenderVideo()'
+theType[35] = 'RenderVideo()'
+theType[36] = 'RenderSoundcloud()'
+theType[37] = 'RenderVideo()'
+theType[38] = 'RenderVideo()'
+theType[39] = 'RenderVideo()'
+theType[40] = 'RenderVideo()'
+theType[41] = 'RenderVideo()'
+theType[42] = 'RenderImage()'
+theType[43] = 'RenderVideo()'
+theType[44] = 'RenderVideo()'
+theType[45] = 'RenderVideo()'
+theType[46] = 'RenderVideo()'
+theType[47] = 'RenderVideo()'
 
 
 var j = 0
@@ -213,8 +213,7 @@ for (i = 0; i < p; i++){
    preBuffer[i].src = theImages[i]
 }
 //it's rigged for now, for the purpose of testing
-var whichImage = 16
-    //Math.round(Math.random()*(p-1));
+var whichImage = Math.round(Math.random()*(p-1));
 
 function showImage(){
     if (theType[whichImage]=='RenderLink()')
@@ -230,10 +229,55 @@ function showImage(){
 function RenderImage(){
 document.getElementById("toggle").innerHTML = '<img src="'+theEmbedCode[whichImage]+'" '+theExtras[whichImage]+' >'
 }
+
 function RenderVideo(){
-document.getElementById("toggle").innerHTML = '<object width="720" height="405"><param name="movie" value="https://www.youtube.com/v/'+theEmbedCode[whichImage]+'?autoplay=1&controls=0&disablekb=1&modestbranding=1&rel=0&showinfo=0&autohide=1&version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowScriptAccess" value="always"></param><embed src="https://www.youtube.com/v/'+theEmbedCode[whichImage]+'?autoplay=1&controls=0&disablekb=1&iv_load_policy=3&modestbranding=1&rel=0'+theExtras[whichImage]+'&showinfo=0&autohide=1&version=3" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always" width="720" height="405"></embed></object>';
+document.getElementById("toggle").innerHTML = '<object width="720" height="405"><param name="movie" value="https://www.youtube.com/v/'+theEmbedCode[whichImage]+'?autoplay=1&controls=0&disablekb=1&modestbranding=1&enablejsapi=1&rel=0&showinfo=0&autohide=1&version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowScriptAccess" value="always"></param><embed id="ytplayer" src="https://www.youtube.com/v/'+theEmbedCode[whichImage]+'?autoplay=1&playerapiid=ytplayer&controls=0&enablejsapi=1&disablekb=1&iv_load_policy=3&modestbranding=1&rel=0'+theExtras[whichImage]+'&showinfo=0&autohide=1&version=3" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always" width="720" height="405"></embed></object>';
+}
+
+function RenderSoundcloud(){
+document.getElementById("toggle").innerHTML = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+theEmbedCode[whichImage]+'&amp;color=00aabb&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'
 }
 
 function DerenderVideo(){
     document.getElementById("toggle").innerHTML = "IGNOREME"
+}
+
+//The Youtube Integration Stuff
+function onYouTubePlayerReady(ytplayer) {
+     ytplayer = document.getElementById("ytplayer");
+  ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
+}
+
+function onytplayerStateChange(newState) {
+ if (newState == 0)
+ {
+     HideStuff();
+    DerenderVideo();
+    startParallax();
+ }
+}
+
+// sandstorm stuff
+var timeoutID;
+
+function DelaySongName(){
+    
+    timeoutID = window.setTimeout(SongName, 34500);
+    document.getElementById("toggle2").removeAttribute("onclick")
+    document.getElementById("toggle2").setAttribute("onclick", "window.location.reload()")
+}
+
+function clearSongName() {
+  window.clearTimeout(timeoutID);
+}
+
+function SongName(){
+        $("<h1>Song Name? Song Name?</h1>").delay( 34000 ).replaceAll("h1");
+        $("<h2> Song Name? Song Name?</h2>").delay( 34000 ).replaceAll("h2");
+            $("<h3> Song Name? Song Name?</h3>").delay( 34000 ).replaceAll("h3");
+            $("<p> Song Name? Song Name?</p>").delay( 34000 ).replaceAll("p");
+    $("<permalink> Song Name? Song Name?</permalink>").delay( 34000 ).replaceAll("permalink");
+    $("<li> Song Name? Song Name?</li>").delay( 34000 ).replaceAll("li");
+$("<description2> Song Name? Song Name?</description2>").delay( 34000 ).replaceAll("description2");
+
 }
